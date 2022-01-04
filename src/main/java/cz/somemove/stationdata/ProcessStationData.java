695d9528d685e801245065ba4730779e286f4185
Josef Petrak <josef.petrak@bileto.com>
2022-01-04 14:05:11 +0100

    Rewrite to Apache POI and latest Apache Jena

--- src/main/java/cz/somemove/stationdata/ProcessStationData.java
+++ src/main/java/cz/somemove/stationdata/ProcessStationData.java
@@ -6,56 +6,69 @@ import org.apache.jena.rdf.model.Resource;
 import org.apache.jena.sparql.vocabulary.FOAF;
 import org.apache.jena.vocabulary.RDF;
 import org.apache.jena.vocabulary.RDFS;
-import java.io.File;
-import java.io.FileOutputStream;
+
 import cz.somemove.stationdata.vocabs.Goodrelations;
 import cz.somemove.stationdata.vocabs.Rail;
 import cz.somemove.stationdata.vocabs.TIO;
-import org.odftoolkit.odfdom.doc.OdfSpreadsheetDocument;
-import org.odftoolkit.odfdom.doc.table.OdfTable;
-import org.odftoolkit.odfdom.doc.table.OdfTableRow;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class ProcessStationData {
     public static void main( String[] args ) throws Exception {
         Model model = ModelFactory.createDefaultModel();
-        
-        OdfSpreadsheetDocument osd = OdfSpreadsheetDocument.loadDocument(new File("data/vlastnik.ods"));
-        OdfTable table = osd.getTableList().get(0);
-        for (OdfTableRow row : table.getRowList()) {
-            if (row.getRowIndex() > 0) {
-                int code = Integer.valueOf(row.getCellByIndex(0).getStringValue()).intValue();
-                String name = row.getCellByIndex(1).getStringValue();
-                String abbr = row.getCellByIndex(2).getStringValue();
-                
-                createOwner(model, code, name, abbr);
+
+        try (InputStream is = new FileInputStream("data/vlastnik.xlsx")) {
+            Workbook workbook = new XSSFWorkbook(is);
+            Sheet sheet = workbook.getSheetAt(0);
+
+            for (Row row : sheet) {
+                if (row.getRowNum() > 0) {
+                    createOwner(
+                        model,
+                        Integer.parseInt(row.getCell(0).getStringCellValue()),
+                        row.getCell(1).getStringCellValue(),
+                        row.getCell(2).getStringCellValue()
+                    );
+                }
             }
         }
-        osd.close();
-        
-        osd = OdfSpreadsheetDocument.loadDocument(new File("data/sr70.ods"));
-        table = osd.getTableList().get(0);
-        for (OdfTableRow row : table.getRowList()) {
-            if (row.getRowIndex() > 0) {
-                int code = Integer.valueOf(row.getCellByIndex(0).getStringValue()).intValue();
-                String name = row.getCellByIndex(1).getStringValue();
-                int type = Integer.valueOf(row.getCellByIndex(9).getStringValue()).intValue();
-                int owner = Integer.valueOf(row.getCellByIndex(17).getStringValue()).intValue();
-                
-                // Filter out non-passenger railway stations
-                if (type == 1 | type == 3 | type == 7 | type == 61 | type == 62 | type == 63 | type == 65 | type == 82) {
-                    createStation(model, code, name, owner);
-                }                
+
+        try (InputStream is = new FileInputStream("data/sr70.xlsx")) {
+            Workbook workbook = new XSSFWorkbook(is);
+            Sheet sheet = workbook.getSheetAt(0);
+
+            for (Row row : sheet) {
+                if (row.getRowNum() > 0) {
+                    int code = Integer.parseInt(row.getCell(0).getStringCellValue());
+                    String name = row.getCell(1).getStringCellValue();
+                    int type = Integer.parseInt(row.getCell(9).getStringCellValue());
+                    int owner = Integer.parseInt(row.getCell(17).getStringCellValue());
+
+                    // Filter out non-passenger railway stations
+                    if (type == 1 | type == 3 | type == 7 | type == 61 | type == 62 | type == 63 | type == 65 | type == 82) {
+                        createStation(model, code, name, owner);
+                    }
+                }
             }
         }
-        
-        FileOutputStream fos = new FileOutputStream(new File("data/output.rdf"));
-        model.setNsPrefix("rail", Rail.NS);
-        model.setNsPrefix("foaf", FOAF.NS);
-        model.setNsPrefix("gr", Goodrelations.NS);
-        model.setNsPrefix("rdfs", RDFS.getURI());
-        model.setNsPrefix("rdf", RDF.getURI());
-        //model.write(fos, "RDF/XML-ABBREV");
-        model.write(fos);
+
+        try (OutputStream os = new FileOutputStream("data/output.rdf")) {
+            model.setNsPrefix("rail", Rail.NS);
+            model.setNsPrefix("foaf", FOAF.NS);
+            model.setNsPrefix("gr", Goodrelations.NS);
+            model.setNsPrefix("rdfs", RDFS.getURI());
+            model.setNsPrefix("rdf", RDF.getURI());
+            model.setNsPrefix("tio", TIO.NS);
+            //model.write(fos, "RDF/XML-ABBREV");
+            model.write(os);
+        }
     }
     
     static void createOwner(Model model, int code, String name, String abbr) {
